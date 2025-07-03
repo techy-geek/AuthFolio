ade19df787de4ee447c3ef2d017a7ae4e3330765
techy-geek <anuragsharma.nits@gmail.com>
2025-07-03 18:48:18 +0530

    HEHEHE....

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 
 import {app} from "./app.js";
 
+app.get('/', (req, res) => {
+  res.send('AuthFolio Backend is running ✅');
+});
+
+
 app.listen(process.env.PORT,()=>{
     console.log(`Server is listening on ${process.env.PORT}`);
 })
